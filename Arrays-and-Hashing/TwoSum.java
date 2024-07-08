ced0e866f18ceb50411ccf874a6de26fe8559e3a
mca265 <mahimchaudhry4@gmail.com>
2024-07-07 19:24:42 -0700

    Two Sum

---
+++ Arrays-and-Hashing/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    int[] array = new int[2];
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i=0;i<nums.length;i++){
+        if(map.containsKey(target-nums[i])){
+            array[0] = map.get(target-nums[i]);
+            array[1] = i;
+            return array;
+        }
+        else{
+            map.put(nums[i],i);
+        }
+    }
+
+    return array;
+    }
+}
+
+/*
+ * Problem Number: 1
+ * Link: https://leetcode.com/problems/two-sum/
+ */
